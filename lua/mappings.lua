dfda7bc94664bc580a3901aba443462631bc031d
Mizuki <mbaba@kanolab.net>
2024-06-03 15:03:25 +0900

    feat: copilot用のキーマップを用意

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,13 @@ local map = vim.keymap.set
 
 map("i", "jj", "<ESC>")
 
+map(
+  "i",
+  "<C-y>",
+  'copilot#Accept("<CR>")',
+  { noremap = true, silent = true, expr = true, script = true, replace_keycodes = false }
+)
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
 map("n", "<leader>h", "^", { desc = "move beginning of line" })
