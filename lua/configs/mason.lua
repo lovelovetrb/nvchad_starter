71a189aff60fe6da96991bad7a00c7666d2da6c4
Mizuki <mbaba@kanolab.net>
2024-06-04 15:09:16 +0900

    feat: lspconfigに対してnvchadのデフォルトの設定が利用できるように変更

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -12,8 +12,14 @@ require("mason-lspconfig").setup {
     "pyright",
   },
 }
+
+local nvchad_defaluts = require("nvchad.configs.lspconfig")
 require("mason-lspconfig").setup_handlers {
   function(server_name)
-    require("lspconfig")[server_name].setup {}
+    require("lspconfig")[server_name].setup {
+      on_attach = nvchad_defaluts.on_attach,
+      capabilities = nvchad_defaluts.capabilities,
+      on_init = nvchad_defaluts.on_init,
+    }
   end,
 }
