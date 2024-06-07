7d3e047004cc24509a4beefeedc7f4dd36910040
Mizuki <mbaba@kanolab.net>
2024-06-07 22:50:46 +0900

    feat: 起動時に.venvがあれば自動でactivateするようなautocmdを設定

--- init.lua
+++ init.lua
@@ -38,4 +38,19 @@ vim.schedule(function()
   require "mappings"
 end)
 
+local function auto_activate_venv()
+  local venv_path = vim.fn.getcwd() .. '/.venv'
+  if vim.fn.isdirectory(venv_path) == 1 then
+    vim.env.VIRTUAL_ENV = venv_path
+    vim.env.PATH = venv_path .. '/bin:' .. vim.env.PATH
+    print('Activated virtual environment: ' .. venv_path)
+  end
+end
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    auto_activate_venv()
+  end
+})
+
 require "configs.mason"
