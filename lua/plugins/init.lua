79c3c6bc8adf5651832eca5db24a974bacccc103
Mizuki <mbaba@kanolab.net>
2024-10-25 22:50:29 +0900

    feat: lazygitの追加

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,4 +11,5 @@ return {
   { require "plugins.reinforcement.mason-lspconfig" },
   { require "plugins.reinforcement.presene" },
   { require "plugins.reinforcement.todo-comments" },
+  { require "plugins.reinforcement.lazygit" }
 }
