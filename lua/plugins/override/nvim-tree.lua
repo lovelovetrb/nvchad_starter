87112bf426f1b007b49986cd93da7aa9d6370fdf
Mizuki <mbaba@kanolab.net>
2024-10-26 05:33:14 +0900

    refactor: 不要な記述を削除

--- lua/plugins/override/nvim-tree.lua
+++ lua/plugins/override/nvim-tree.lua
@@ -1,9 +1,5 @@
 return {
   "nvim-tree/nvim-tree.lua",
   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-  opts = function()
-    local cfg = require "nvchad.configs.nvimtree"
-    cfg["git"]["ignore"] = false
-    return cfg
-  end,
+  opts = require "nvchad.configs.nvimtree"
 }
