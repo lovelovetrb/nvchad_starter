3dfd9f71887e854a988aab19dd3d1b8a102f19db
Mizuki <mbaba@kanolab.net>
2024-11-13 17:06:04 +0900

    feat: LazyGitに対するキーバインドを追加

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,6 +2,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- ### Insert Mode
 map("i", "jj", "<ESC>")
 
 map(
@@ -11,6 +12,7 @@ map(
   { noremap = true, silent = true, expr = true, script = true, replace_keycodes = false }
 )
 
+-- ### Normal Mode
 map("n", "<leader>h", "^", { desc = "move beginning of line" })
 map("n", "<leader>l", "$", { desc = "move ending of line" })
 
@@ -20,6 +22,10 @@ map("n", "<C-f>", function()
   require("conform").format { lsp_fallback = true }
 end, { desc = "formatting" })
 
+map("n", "<leader>t", '<cmd>LazyGit<CR>', { desc = "lazygit" })
+
+-- ### Visual Mode
 map("v", "v", "$h", { desc = "行末まで選択" })
 
+-- ### Terminal Mode
 map("t", "qq", vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true))
