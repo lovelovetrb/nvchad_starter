652a154422bd687887cfbc0a471db33db64d3c07
Mizuki Baba <info@lovelovetrb.com>
2025-07-15 18:08:12 +0900

    fix: Mason2.0以降の記法に変更

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -1,44 +1,53 @@
 require("mason").setup()
-require("mason-lspconfig").setup {}
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+
+    "html",
+    "cssls",
+    "tailwindcss",
+    "ts_ls",
+    "eslint",
+    "biome",
+
+    "pylsp",
+    "pyrefly",
+
+    "clangd",
+  },
+  automatic_enable = true,
+})
 
 local nvchad_defaults = require "nvchad.configs.lspconfig"
-local custom_handlers = {
-  pylsp = function()
-    require("lspconfig").pylsp.setup {
-      on_attach = nvchad_defaults.on_attach,
-      capabilities = nvchad_defaults.capabilities,
-      on_init = nvchad_defaults.on_init,
-      settings = {
-        pylsp = {
-          plugins = {
-            pycodestyle = { enabled = false },
-            pylint = { enabled = false },
-            flake8 = { enabled = false },
-            autopep8 = { enabled = false },
-            yapf = { enabled = false },
-            ruff = {
-              enabled = true, -- Enable the plugin
-              formatEnabled = true, -- Enable formatting using ruffs formatter
-              extendSelect = { "I" }, -- Rules that are additionally used by ruff
-              format = { "I" }, -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
-            },
-          },
+
+-- Custom configuration for pylsp using the new vim.lsp.config API
+vim.lsp.config('pylsp', {
+  on_attach = nvchad_defaults.on_attach,
+  capabilities = nvchad_defaults.capabilities,
+  on_init = nvchad_defaults.on_init,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = { enabled = false },
+        pylint = { enabled = false },
+        flake8 = { enabled = false },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        ruff = {
+          enabled = true,         -- Enable the plugin
+          formatEnabled = true,   -- Enable formatting using ruffs formatter
+          extendSelect = { "I" }, -- Rules that are additionally used by ruff
+          format = { "I" },       -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
         },
       },
-    }
-  end,
-}
+    },
+  },
+})
 
-require("mason-lspconfig").setup_handlers {
-  function(server_name)
-    if custom_handlers[server_name] then
-      custom_handlers[server_name]()
-    else
-      require("lspconfig")[server_name].setup {
-        on_attach = nvchad_defaults.on_attach,
-        capabilities = nvchad_defaults.capabilities,
-        on_init = nvchad_defaults.on_init,
-      }
-    end
-  end,
-}
+-- For other servers, use the wildcard to apply default settings
+vim.lsp.config('*', {
+  on_attach = nvchad_defaults.on_attach,
+  capabilities = nvchad_defaults.capabilities,
+  on_init = nvchad_defaults.on_init,
+})
+
